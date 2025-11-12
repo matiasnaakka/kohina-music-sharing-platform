0cc2c331380c1d7b2914d109431b342c50a1926c
matiasnaakka <matias5230@gmail.com>
2025-11-12 14:09:28 +0200

    added comment

--- my-app/src/supabaseclient.js
+++ my-app/src/supabaseclient.js
@@ -6,6 +6,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
+//creates supabase client
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
