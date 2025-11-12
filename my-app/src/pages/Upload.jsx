30a0920cb9d692e9215f7fe30c97cb14bbdb67e3
matiasnaakka <matias5230@gmail.com>
2025-11-12 14:29:57 +0200

    muutokset

--- my-app/src/pages/Upload.jsx
+++ my-app/src/pages/Upload.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { supabase, getPublicStorageUrl } from '../supabaseclient'
 import NavBar from '../components/NavBar'
 import AddToPlaylist from '../components/AddToPlaylist'
