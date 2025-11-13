1a68a824bbb840d08501bfc88ed897625ff744a9
matiasnaakka <matias5230@gmail.com>
2025-11-13 12:20:26 +0200

    code changes

--- my-app/src/components/UserProfile.jsx
+++ my-app/src/components/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { supabase } from '../supabaseclient'
 
 const UserProfile = ({ session, isModal = false, onClose, readOnly = false }) => {
