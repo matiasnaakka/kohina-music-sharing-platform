f9d5739a358535fa093d10fc32642b9644c4b558
matiasnaakka <matias5230@gmail.com>
2026-01-19 22:18:29 +0200

    feat: add Google as an authentication provider in Auth component

--- my-app/src/Router.jsx
+++ my-app/src/Router.jsx
@@ -114,7 +114,7 @@ const Routing = ({ player }) => {
                   <Auth
                     supabaseClient={supabase}
                     appearance={customAppearance}
-                    providers={[]}
+                    providers={['google']}
                     // Ensures external auth flows return into the SPA route on Cloudflare Pages
                     redirectTo={redirectToHome}
                   />
